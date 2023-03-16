3f1cbd3ecef2f721e02b8570e650a0bdff0d96cc
Ali Bakhshi <ali.bakhshi.office@gmail.com>
2023-03-16 18:05:46 +0330

    save catid for products table

---
+++ src/app/httpService.js
@@ -0,0 +1,26 @@
+const Agent = require("agentkeepalive");
+const HttpsAgent = require("agentkeepalive").HttpsAgent;
+
+const keepAliveAgent = new Agent({
+  maxSockets: 160,
+  maxFreeSockets: 160,
+  timeout: 60000,
+  freeSocketTimeout: 30000,
+  keepAliveMsecs: 60000,
+});
+
+const httpsKeepAliveAgent = new HttpsAgent({
+  maxSockets: 160,
+  maxFreeSockets: 160,
+  timeout: 60000,
+  freeSocketTimeout: 30000,
+  keepAliveMsecs: 60000,
+});
+
+const httpService = axios.create({
+  //   baseURL: config.domain,
+  httpAgent: keepAliveAgent,
+  httpsAgent: httpsKeepAliveAgent,
+});
+
+export default httpService;
--- src/startScraping.js
+++ src/startScraping.js
@@ -1,4 +1,3 @@
-const { default: axios } = require("axios");
 const cheerio = require("cheerio");
 const { logger } = require("./app/logger");
 const { Category } = require("./models/Category");
@@ -13,104 +12,102 @@ async function startScraping(scrapeProducts, scrapeWithDetails) {
     return new Promise((resolve) => setTimeout(resolve, time));
   }
 
-  axios
+  async function getProductsByCatUrl(catId, url, pageNumber = 1) {
+    // console.log("url2", url);
+    let totalPages = 1;
+    await delay(defaultDelay);
+    await httpService
+      .get(url + "page/" + pageNumber)
+      .then((response) => {
+        const $ = cheerio.load(response.data);
+        totalPages = Number(
+          $(".page-numbers").find("li:nth-last-child(2) > a").text()
+        );
+        $(".product-grid-item").each(async (index, product) => {
+          if (!scrapeWithDetails) {
+            Product.createIfNotExist({
+              category_id: catId,
+              title: $(product).find(".wd-entities-title").text(),
+              price:
+                ($(product).find(".price > span > bdi").length
+                  ? $(product).find(".price > span > bdi").text()
+                  : $(product).find(".price > del > span > bdi").text()
+                ).replace(/[^0-9\.]+/g, "") || 0,
+              url: $(product).find(".wd-entities-title > a").attr("href"),
+            });
+          } else {
+            await delay(defaultDelay);
+            await httpService
+              .get($(product).find(".product-element-top > a").attr("href"))
+              .then((response) => {
+                const $ = cheerio.load(response.data);
+                Product.createIfNotExist({
+                  category_id: catId,
+                  title: $(".product-image-summary .product_title").text(),
+                  price: $(".product-image-summary .price bdi").length
+                    ? $(".product-image-summary .price bdi").text()
+                    : $(".product-image-summary .price del").text(),
+                  description: $(
+                    "woocommerce-product-details__short-description > p"
+                  ).text(),
+                  entitle: $(
+                    ".product-image-summary .english-name-product"
+                  ).text(),
+                  code: $(".product-image-summary .sku_wrapper .sku").text(),
+                  export_code: $(
+                    ".product-image-summary .barcode-varedati"
+                  ).text(),
+                  url: $(product).find(".product-element-top > a").attr("href"),
+                });
+              })
+              .catch((error) => {
+                console.error(error);
+              });
+          }
+        });
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+
+    if (pageNumber < totalPages) {
+      await delay(defaultDelay);
+      getProductsByCatUrl(catId, url, pageNumber + 1);
+    }
+  }
+
+  function getSubMenuItems(menuItem, parentId = null) {
+    const subMenuItems = $(menuItem).find("ul").eq(0).find("> li");
+    if (subMenuItems?.length) {
+      subMenuItems.each((_, subItem) => {
+        // console.log("sub", $(subItem).find(" > a").text());
+        Category.createIfNotExist({
+          title: $(subItem).find(" > a").text(),
+          parent_id: parentId,
+          url: $(subItem).find(" > a").attr("href"),
+        })
+          .then((result) => {
+            getSubMenuItems(subItem, result.id);
+          })
+          .catch((error) => {
+            console.error(error);
+          });
+      });
+    } else {
+      console.log(
+        "products of category => ",
+        $(menuItem).find("> a").attr("href")
+      );
+      getProductsByCatUrl(parentId, $(menuItem).find("> a").attr("href"), 1);
+    }
+  }
+
+  httpService
     .get("https://mosbatesabz.com/")
     .then((response) => {
       console.log("status", response.status);
       // return;
       const $ = cheerio.load(response.data);
-
-      async function getProductsByCatUrl(url, pageNumber = 1) {
-        // console.log("url2", url);
-        let totalPages = 1;
-        await delay(defaultDelay);
-        await axios
-          .get(url + "page/" + pageNumber)
-          .then((response) => {
-            const $ = cheerio.load(response.data);
-            totalPages = Number(
-              $(".page-numbers").find("li:nth-last-child(2) > a").text()
-            );
-            $(".product-grid-item").each(async (index, product) => {
-              if (!scrapeWithDetails) {
-                Product.createIfNotExist({
-                  title: $(product).find(".wd-entities-title").text(),
-                  price:
-                    ($(product).find(".price > span > bdi").length
-                      ? $(product).find(".price > span > bdi").text()
-                      : $(product).find(".price > del > span > bdi").text()
-                    ).replace(/[^0-9\.]+/g, "") || 0,
-                  url: $(product).find(".wd-entities-title > a").attr("href"),
-                });
-              } else {
-                await delay(defaultDelay);
-                await axios
-                  .get($(product).find(".product-element-top > a").attr("href"))
-                  .then((response) => {
-                    const $ = cheerio.load(response.data);
-                    Product.createIfNotExist({
-                      title: $(".product-image-summary .product_title").text(),
-                      price: $(".product-image-summary .price bdi").length
-                        ? $(".product-image-summary .price bdi").text()
-                        : $(".product-image-summary .price del").text(),
-                      description: $(
-                        "woocommerce-product-details__short-description > p"
-                      ).text(),
-                      entitle: $(
-                        ".product-image-summary .english-name-product"
-                      ).text(),
-                      code: $(
-                        ".product-image-summary .sku_wrapper .sku"
-                      ).text(),
-                      export_code: $(
-                        ".product-image-summary .barcode-varedati"
-                      ).text(),
-                      url: $(product)
-                        .find(".product-element-top > a")
-                        .attr("href"),
-                    });
-                  })
-                  .catch((error) => {
-                    console.error(error);
-                  });
-              }
-            });
-          })
-          .catch((error) => {
-            console.error(error);
-          });
-
-        if (pageNumber < totalPages) {
-          await delay(defaultDelay);
-          getProductsByCatUrl(url, pageNumber + 1);
-        }
-      }
-
-      function getSubMenuItems(menuItem, parentId = null) {
-        const subMenuItems = $(menuItem).find("ul").eq(0).find("> li");
-        if (subMenuItems?.length) {
-          subMenuItems.each((_, subItem) => {
-            // console.log("sub", $(subItem).find(" > a").text());
-            Category.createIfNotExist({
-              title: $(subItem).find(" > a").text(),
-              parent_id: parentId,
-              url: $(subItem).find(" > a").attr("href"),
-            })
-              .then((result) => {
-                getSubMenuItems(subItem, result.id);
-              })
-              .catch((error) => {
-                console.error(error);
-              });
-          });
-        } else {
-          console.log(
-            "products of category => ",
-            $(menuItem).find("> a").attr("href")
-          );
-          getProductsByCatUrl($(menuItem).find("> a").attr("href"), 1);
-        }
-      }
 
       const menu = $(".whb-header-bottom .wd-header-nav > ul.menu");
 
